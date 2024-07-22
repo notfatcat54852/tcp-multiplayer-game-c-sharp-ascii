73216297b92909ecec5fae2c97b26538c7d67158
notfatcat54852 <figgythekid@gmail.com>
2024-07-22 13:11:00 +1200

    fgd

--- server/Program.cs
+++ server/Program.cs
@@ -1,1 +1,23 @@
-﻿hello world
+﻿using System;
+using System.Threading;
+
+class Program
+{
+    static void Main()
+    {
+        string message = "hello world";
+
+        // Create and start a thread with a method that takes a generic parameter
+        Thread thread = new Thread(() => PrintMessage(message));
+        thread.Start();
+
+        // Wait for the thread to finish
+        thread.Join();
+    }
+
+    // Method that the thread will execute
+    static void PrintMessage(string message)
+    {
+        Console.WriteLine(message);
+    }
+}
