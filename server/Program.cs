abbf63feebe22155de1a01a509e2939b37cd0142
notfatcat54852 <figgythekid@gmail.com>
2024-08-06 10:35:00 +1200

    the issue with the licents recieving mutliple messges each was due to the thread being initialized for each client thread made instead of in the main thread

--- server/Program.cs
+++ server/Program.cs
@@ -1,6 +1,5 @@
 ﻿using System;
 using System.Collections.Generic;
-using System.IO;
 using System.Net;
 using System.Net.Sockets;
 using System.Text;
@@ -8,21 +7,22 @@ using System.Threading;
 
 class TcpServer
 {
-    //create client list with lock for broadcast feature    
     private static List<TcpClient> clients = new List<TcpClient>();
     private static object clientsLock = new object();
+    private static bool isBroadcastLoopRunning = false;
+
     static void Main()
     {
-        // Define the server port
         int port = 13000;
         TcpListener server = new TcpListener(IPAddress.Any, port);
-
         server.Start();
         Console.WriteLine("Server started and waiting for connection...");
+
+        // Start the broadcast loop once
+        StartBroadcastLoop();
+
         while (true)
         {
-
-            // Accept client connection
             TcpClient client = server.AcceptTcpClient();
             Console.WriteLine("Client connected!");
 
@@ -31,84 +31,95 @@ class TcpServer
                 clients.Add(client);
             }
 
-            // Create a thread to handle the client
             Thread clientThread = new Thread(HandleClient);
             clientThread.Start(client);
         }
+    }
 
-        // Keep the server running
-        Console.ReadLine();
+    static void StartBroadcastLoop()
+    {
+        lock (clientsLock)
+        {
+            if (!isBroadcastLoopRunning)
+            {
+                isBroadcastLoopRunning = true;
+                Thread broadcastLoopTest = new Thread(BroadcastLoopTest);
+                broadcastLoopTest.Start();
+            }
+        }
     }
 
     static void HandleClient(object obj)
     {
         TcpClient client = (TcpClient)obj;
-
-        // Get the stream to read/write data
         NetworkStream stream = client.GetStream();
 
-        Thread clientThreadListen = new Thread(HandleClientListen);
-        clientThreadListen.Start(client);
-
-        //// Read data from the client
-        //byte[] buffer = new byte[256];
-        //int bytesRead = stream.Read(buffer, 0, buffer.Length);
-        //string receivedMessage = Encoding.ASCII.GetString(buffer, 0, bytesRead);
-        //Console.WriteLine("Received: " + receivedMessage);
-
-        Thread clientThreadTalk = new Thread(HandleClientTalk);
-        clientThreadTalk.Start(client);
-
-        //BroadcastLoopTest thread
-        Thread broadcastLoopTest = new Thread(BroadcastLoopTest);
-        broadcastLoopTest.Start();
-
-        //// Send a response to the client
-        //string responseMessage = "Hello from server!";
-        //byte[] responseBytes = Encoding.ASCII.GetBytes(responseMessage);
-        //stream.Write(responseBytes, 0, responseBytes.Length);
-
-        clientThreadListen.Join();
-        clientThreadTalk.Join();
-        //for testing the bc test
-        broadcastLoopTest.Join();
-
-
-        // Close the connection
-        client.Close();
-
-        lock (clientsLock)
+        try
         {
-            clients.Remove(client);
+            Thread clientThreadListen = new Thread(HandleClientListen);
+            clientThreadListen.Start(client);
+
+            Thread clientThreadTalk = new Thread(HandleClientTalk);
+            clientThreadTalk.Start(client);
+
+            clientThreadListen.Join();
+            clientThreadTalk.Join();
+        }
+        catch (Exception ex)
+        {
+            Console.WriteLine("Client error: " + ex.Message);
+        }
+        finally
+        {
+            lock (clientsLock)
+            {
+                clients.Remove(client);
+            }
+            client.Close();
+            Console.WriteLine("Client disconnected.");
         }
     }
-
 
     static void HandleClientListen(object obj)
     {
         TcpClient client = (TcpClient)obj;
-
-        // Get the stream to read/write data
         NetworkStream stream = client.GetStream();
 
-        // Read data from the client
-        byte[] buffer = new byte[256];
-        int bytesRead = stream.Read(buffer, 0, buffer.Length);
-        string receivedMessage = Encoding.ASCII.GetString(buffer, 0, bytesRead);
-        Console.WriteLine("Received: " + receivedMessage);
+        try
+        {
+            byte[] buffer = new byte[256];
+            int bytesRead;
+            while ((bytesRead = stream.Read(buffer, 0, buffer.Length)) != 0)
+            {
+                string receivedMessage = Encoding.ASCII.GetString(buffer, 0, bytesRead);
+                Console.WriteLine("Received: " + receivedMessage);
+
+                BroadcastMessage(receivedMessage);
+            }
+        }
+        catch (Exception ex)
+        {
+            Console.WriteLine("Listen error: " + ex.Message);
+        }
     }
+
     static void HandleClientTalk(object obj)
     {
         TcpClient client = (TcpClient)obj;
-
-        // Get the stream to read/write data
         NetworkStream stream = client.GetStream();
 
-        // Send a response to the client
-        string responseMessage = "Hello from server!";
-        byte[] responseBytes = Encoding.ASCII.GetBytes(responseMessage);
-        stream.Write(responseBytes, 0, responseBytes.Length);
+        try
+        {
+            string responseMessage = "Hello from server!";
+            byte[] responseBytes = Encoding.ASCII.GetBytes(responseMessage);
+            stream.Write(responseBytes, 0, responseBytes.Length);
+        }
+        catch (Exception ex)
+        {
+            Console.WriteLine("Talk error: " + ex.Message);
+        }
     }
+
     static void BroadcastLoopTest()
     {
         int count = 0;
@@ -125,10 +136,26 @@ class TcpServer
         lock (clientsLock)
         {
             byte[] responseBytes = Encoding.ASCII.GetBytes(message);
+            List<TcpClient> disconnectedClients = new List<TcpClient>();
+
             foreach (var client in clients)
             {
-                NetworkStream stream = client.GetStream();
-                stream.Write(responseBytes, 0, responseBytes.Length);
+                try
+                {
+                    NetworkStream stream = client.GetStream();
+                    stream.Write(responseBytes, 0, responseBytes.Length);
+                }
+                catch (Exception)
+                {
+                    disconnectedClients.Add(client);
+                }
+            }
+
+            foreach (var client in disconnectedClients)
+            {
+                clients.Remove(client);
+                client.Close();
+                Console.WriteLine("Removed disconnected client.");
             }
         }
     }
