273d6052718a0d0491dacba02c32e5e1e8d54194
notfatcat54852 <figgythekid@gmail.com>
2024-08-07 15:57:12 +1200

    removed the draw and split tuple stuff cause i dont know how to get it to work xdddd

--- client/Program.cs
+++ client/Program.cs
@@ -1,12 +1,33 @@
 ﻿using System;
-
 using System.Net.Sockets;
 
 using System.Text;
 using System.Threading;
 
+using System.Collections.Generic;
+
+
 class TcpClientProgram
 {
+    //private class ClientInfo
+    //{
+    //    public TcpClient Client { get; set; }
+    //    public int X { get; set; }
+    //    public int Y { get; set; }
+    //    public int ID { get; set; }
+
+    //    private static int nextId = 1; // Static counter for client IDs
+
+    //    public ClientInfo(TcpClient client, int x, int y, int id)
+    //    {
+    //        Client = client;
+    //        X = x;
+    //        Y = y;
+    //        ID = id;
+    //    }
+    //}
+    private static object playersLock = new object();
+    private static string playerString;
     static void Main()
     {
         // Define the server address and port
@@ -25,16 +46,72 @@ class TcpClientProgram
         Thread listenToServer = new Thread(ListenToServer);
         listenToServer.Start(client);
         ///
+        //Thread draw = new Thread(Draw);
+        //draw.Start();
+        ////
 
         listenToServer.Join();
         talkToServer.Join();
 
-        Console.ReadLine();
+
+        //draw.Join();
 
         // Close the connection
         client.Close();
-    }
 
+        Console.ReadKey();
+    }
+    //static void Draw()
+    //{
+    //    string x;
+    //    lock (playersLock)
+    //    {
+    //        x = playerString;
+    //    }
+    //    if (x[0] != 'p')
+    //        return;
+    //    string[] x2 = x.Split(' ');
+    //    List<Tuple<int, int>> pairs = new List<Tuple<int, int>>();
+    //    pairs.Add(Tuple.Create(1, 2));
+    //    pairs.Add(Tuple.Create(4, 5));
+    //    while (true)
+    //    {
+    //        for (int i = 0; i < 10; i++)
+    //        {
+    //            for (int j = 0; j < 10; j++)
+    //            {
+    //                if (pairs.Contains(Tuple.Create(i, j)))
+    //                {
+    //                    Console.WriteLine($"#");
+    //                }
+    //                else
+    //                {
+    //                    Console.Write(" ");
+    //                }
+    //            }
+    //            Console.WriteLine();
+    //        }
+    //        Thread.Sleep(1000);
+    //    }
+    //}
+    public static int playerCount = 0;
+    public static void UpdatePlayers(string x)
+    {
+        lock (playersLock)
+        {
+            playerString = x;
+        }
+        if (x[0] != 'p')
+            return;
+        string[] x2 = x.Split(' ');
+        playerCount = x2.Length / 3;
+        Console.WriteLine(playerCount);
+
+    }
+    static void RemoveClientById(int id)
+    {
+        //lol
+    }
     static void ListenToServer(object obj)
     {
         TcpClient client = (TcpClient)obj;
@@ -48,9 +125,10 @@ class TcpClientProgram
             string responseMessage = Encoding.ASCII.GetString(buffer, 0, bytesRead);
             Console.WriteLine("Received: " + responseMessage);
 
+            UpdatePlayers(responseMessage);
+            //UpdatePlayers(responseMessage);
         }
     }
-
     static void TalkToServer(object obj)
     {
         TcpClient client = (TcpClient)obj;
